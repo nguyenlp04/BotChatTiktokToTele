5fd389ec7c1dc04e5e11b9c314f9521fea6ea191
nguyenlp04 <lphuonguye.ecn.2182004@gmail.com>
2025-11-14 12:21:28 +0700

    Fix Railway deployment: use PORT from environment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const OTPAuth = require('otpauth');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
